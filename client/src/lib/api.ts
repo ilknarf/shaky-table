263a90436f0300d6bd8b22eb83efe035959234d3
Frank <lifrank@grinnell.edu>
2021-09-18 22:58:51 -0500

    update client-side code

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,8 +1,20 @@
-const API_ROOT = "https://localhost/api/v1";
+const API_ROOT_V1 = "https://localhost/api/v1";
 
 export const createAccount = async (data: URLSearchParams): Promise<Response> => (
-    fetch(`${API_ROOT}/create_account`, {
+    fetch(`${API_ROOT_V1}/create_account`, {
         method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-type": "application/x-www-form-urlencoded",
+        },
+        body: data,
+}));
+
+export const loginUser = async (data: URLSearchParams): Promise<Response> => (
+    fetch(`${API_ROOT_V1}/login`, {
+        method: "POST",
+        // allow cookies to attach
+        credentials: "include",
         headers: {
             "Accept": "application/json",
             "Content-type": "application/x-www-form-urlencoded",
